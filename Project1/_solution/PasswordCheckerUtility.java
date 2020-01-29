44acd6c81226ad5b2648a22db0da0642eb6557ef
Alec M <alecmattu1234@gmail.com>
2020-01-29 08:57:05 -0500

    Added PasswordCheckerUtility.java

---
+++ Project1/_solution/PasswordCheckerUtility.java
@@ -0,0 +1,51 @@
+// Namespace
+package _solution;
+
+// Imports
+import java.util.ArrayList;
+
+/**
+ * @author Alec M.
+ * @date 01/29/2020
+ * @version 0.01
+ */
+public class PasswordCheckerUtility {
+	/**
+	 * Returns true for valid passwords, throws errors for invalid passwords.
+	 * 
+	 * @param String password
+	 * @author Alec M.
+	 * @date 01/29/2020
+	 * @version 0.01
+	 * @throws LengthException, NoDigitException, NoUpperAlphaException, NoLowerAlphaException, InvalidSequenceException
+	 */
+	public static boolean isValidPassword(String passwordString) {
+		return false;
+	}
+
+	/**
+	 * Returns true for valid passwords, false for weak passwords
+	 * 
+	 * @param String password
+	 * @author Alec M.
+	 * @date 01/29/2020
+	 * @version 0.01
+	 * @throws None
+	 */
+	public static boolean isWeakPassword(String passwordString) {
+		return false;
+	}
+	
+	/**
+	 * Returns a arraylist of valid passwords
+	 * 
+	 * @param ArrayList password
+	 * @author Alec M.
+	 * @date 01/29/2020
+	 * @version 0.01
+	 * @throws None
+	 */	
+	public static ArrayList<String> invalidPassword(ArrayList<String> passwords) {
+		return new ArrayList<String>();
+	}
+}
