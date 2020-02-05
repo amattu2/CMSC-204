170bcbdf35f0c046691741ee837133e704bec23e
Alec M <alecmattu1234@gmail.com>
2020-02-05 09:19:19 -0500

    Finished Lab2 (GenericsLab)

---
+++ Lab2/DataSetGen.java
@@ -0,0 +1,51 @@
+/**
+   Computes the average of a set of data values.
+*/
+public class DataSetGen<T>
+{
+   private double sum;
+   private Measurable maximum;
+   private int count;
+
+   /**
+      Constructs an empty data set.
+ * @return 
+   */
+   public DataSetGen()
+   {
+      sum = 0;
+      count = 0;
+      maximum = null;
+   }
+
+   /**
+      Adds a data value to the data set.
+      @param x a data value
+   */
+   public void add(Measurable x)
+   {
+      sum = sum + x.getMeasure();
+      if (count == 0 || maximum.getMeasure() < x.getMeasure())
+         maximum = x;
+      count++;
+   }
+
+   /**
+      Gets the average of the added data.
+      @return the average or 0 if no data has been added
+   */
+   public double getAverage()
+   {
+      if (count == 0) return 0;
+      else return sum / count;
+   }
+
+   /**
+      Gets the largest of the added data.
+      @return the maximum or 0 if no data has been added
+   */
+   public T getMaximum()
+   {
+      return (T) maximum;
+   }
+}
