354458f4233c9b33406aeadc106998a4154a0dd1
Alec M <alecmattu1234@gmail.com>
2020-02-11 17:02:55 -0500

    Added Customer.java and LinkedQueue.java implementations

---
+++ Lab4/Customer.java
@@ -0,0 +1,30 @@
+/**
+ * A ADT bag customer implementation
+ * 
+ * @author Alec
+ * @date 02/11/2020
+ * @version 0.01
+ */
+public class Customer {
+	// Variables
+	private int clock, transactionTime, numberOfArrivals;
+	
+	// Constructor
+	public Customer(int clock, int transactionTime, int numberOfArrivals) {
+		this.clock = clock;
+		this.transactionTime = transactionTime;
+		this.numberOfArrivals = numberOfArrivals;
+	}
+	
+	public int getTransactionTime() {
+		return this.transactionTime;
+	}
+
+	public int getArrivalTime() {
+		return this.numberOfArrivals;
+	}
+
+	public String getCustomerNumber() {
+		return Integer.toString(this.numberOfArrivals);
+	}
+}
---
+++ Lab4/LinkedQueue.java
@@ -0,0 +1,99 @@
+/**
+ * A LinkedQueue implementation based on Pearson Module #8 - Queues
+ * 
+ * @author Alec M.
+ * @date 02/11/2020
+ * @version 0.03
+ */
+public final class LinkedQueue<T> implements QueueInterface<T> {
+	// Class Variables
+	private Node firstnode = null;
+	private Node lastnode = null;
+	
+	@Override
+	public void enqueue(T newEntry) {
+		// Variables
+		Node newNode = new Node(newEntry);
+		
+		// Checks
+		if (isEmpty()) {
+			firstnode = newNode;
+		} else {
+			lastnode.setNextNode(newNode);
+		}
+		
+		lastnode = newNode;
+	}
+
+	@Override
+	public T dequeue() {
+		// Variables
+		T front = null;
+		
+		// Try
+		try {
+			front = this.getFront();
+			firstnode.setData(front);
+			firstnode = firstnode.getNextNode();
+		} catch(Exception e) {}
+		
+		// Checks
+		if (firstnode == null) {
+			lastnode = null;
+		}
+		
+		// Return
+		return front;
+	}
+
+	@Override
+	public T getFront() {
+		// Checks
+		if (this.isEmpty()) {
+			return null;
+		} else {
+			return firstnode.getData();
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return firstnode == null && lastnode == null;
+	}
+
+	@Override
+	public void clear() {
+		firstnode = null;
+		lastnode = null;
+	}
+	
+	private class Node {
+		// Variables
+		private T data;
+		private Node next;
+		
+		// Constructor
+		public Node(T newNode) {
+			data = newNode;
+		}
+		
+		public void setNextNode(Node nextNode) {
+			next = nextNode;
+		}
+
+		public void setData(T newData) {
+			data = newData;
+			
+		}
+
+		public LinkedQueue<T>.Node getNextNode() {
+			return next;
+		}
+
+		public T getData() {
+			return data;
+		}
+		
+	}
+
+}
