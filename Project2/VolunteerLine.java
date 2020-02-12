a1c744a9258b372d0450ea8ab65654870f7742a5
Alec M <alecmattu1234@gmail.com>
2020-02-12 15:23:06 -0500

    Added size constructor

--- Project2/VolunteerLine.java
+++ Project2/VolunteerLine.java
@@ -1,6 +1,10 @@
 
 public class VolunteerLine implements VolunteerLineInterface {
 
+	public VolunteerLine(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public boolean addNewVolunteer(Volunteer v) throws VolunteerException {
 		// TODO Auto-generated method stub
