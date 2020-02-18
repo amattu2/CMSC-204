7b1bbed594712181c8e5f834d58ae7487047c49a
Alec M <alecmattu1234@gmail.com>
2020-02-18 15:36:27 -0500

    Added documentation

--- Lab5/ArraySum.java
+++ Lab5/ArraySum.java
@@ -1,9 +1,32 @@
+/*
+ * A class to manage array calculations
+ * 
+ * @author Alec M
+ * @date 02/18/2020
+ * @version 0.01
+ */
 public class ArraySum {
+	/**
+	 * Calculate the sum of an array
+	 * 
+	 * @param Integer array
+	 * @param Integer size of array
+	 * @return Integer sum of array
+	 */
 	public int sumOfArray(Integer a[], int size) {
 		// Return
 		return sum(a, size, 0, 0);
 	}
 	
+	/**
+	 * Recursive method to calculate the running sum of an array
+	 * 
+	 * @param Integer array
+	 * @param Integer size of array
+	 * @param Integer current index in array
+	 * @param Integer current total of array (0-to-index)
+	 * @return Integer sum of array (0-to-index)
+	 */
 	private int sum(Integer a[], int size, int index, int total) {
 		if (index > size) {
 			return total;
--- Lab5/Fibonacci.java
+++ Lab5/Fibonacci.java
@@ -1,4 +1,17 @@
+/*
+ * A class to manage Fibonacci sequence calculations
+ * 
+ * @author Alec M
+ * @date 02/18/2020
+ * @version 0.01
+ */
 public class Fibonacci {
+	/**
+	 * Calculate the Fibonacci sequence for a number
+	 * 
+	 * @param Integer number
+	 * @return Long calculated number
+	 */
 	public static long calculate(int num) {
 		// Checks
 		if (num == 1 || num == 2) {
