6d297d6d5c4e7c30a8498b507f9f8fb6c4a64f31
Alec M <alecmattu1234@gmail.com>
2020-02-26 10:44:09 -0500

    Added method stubs

---
+++ Project3/BasicDoubleLinkedList.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.ListIterator;
+
+/**
+ * @author Alec M.
+ * @date 02/26/2020
+ * @version 0.01a
+ */
+public class BasicDoubleLinkedList<T> {
+
+	public void addToFront(String newElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<String> toArrayList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addToEnd(String newElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String retrieveFirstElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String retrieveLastElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <StringComparator> void remove(String newElement, StringComparator sComp) {
+		throw new UnsupportedOperationException("Not supported");
+	}
+
+	public ListIterator<String> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
