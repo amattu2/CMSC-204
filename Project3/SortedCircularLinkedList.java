a0a4814907febac0577ce3a303aff0ef63766ae3
Alec <Alec@amattu>
2020-03-07 11:59:59 -0500

    Fixed type declaration

--- Project3/SortedCircularLinkedList.java
+++ Project3/SortedCircularLinkedList.java
@@ -1,7 +1,5 @@
 // Imports
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.ListIterator;
 
 /**
  * A CIRCULAR singly-linked implementation
@@ -10,53 +8,8 @@ import java.util.ListIterator;
  * @date 02/26/2020
  * @version 0.01a
  */
-public class SortedCircularLinkedList<T> {
+public class SortedCircularLinkedList<T> extends BasicCircularLinkedList<T> {
 	public SortedCircularLinkedList(Comparator<T> c) {
 		// TODO Auto-generated constructor stub
 	}
-
-	public ArrayList<String> toArrayList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String retrieveFirstElement() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String retrieveLastElement() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getLast() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getFirst() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void remove(T newElement, Comparator<T> c) {
-		// TODO Auto-generated method stub
-	}
-
-	public void add(String newElement) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public ListIterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void addToEnd(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
