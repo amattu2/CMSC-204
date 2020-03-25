12be44f700a010084bd6692cf3c3c9a41eede408
Alec M <alecmattu1234@gmail.com>
2020-03-25 09:33:04 -0400

    Implemented interface shells

---
+++ Project4/ConcordanceDataStructure.java
@@ -0,0 +1,12 @@
+
+public class ConcordanceDataStructure implements ConcordanceDataStructureInterface {
+
+	public ConcordanceDataStructure(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConcordanceDataStructure(String string, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
