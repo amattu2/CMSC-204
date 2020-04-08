d68dfc8591ea35cda73914389fd46c59a8ca0352
Alec M <alecmattu1234@gmail.com>
2020-04-08 08:24:30 -0400

    Implemented TreeNode

--- Project5/TreeNode.java
+++ Project5/TreeNode.java
@@ -7,5 +7,36 @@
  * @see TreeNode(3).html
  */
 public class TreeNode<T> {
-
+	// Class Variables
+	protected T data = null;
+	
+	/**
+	 * Class Constructor
+	 * 
+	 * @param T data
+	 * @throws None
+	 */
+	public TreeNode(T d) {
+		this.data = d;
+	}
+	
+	/**
+	 * Class Constructor
+	 *
+	 * @param T TreeNode
+	 * @throws None
+	 */
+	public TreeNode(TreeNode<T> node) {
+		this.data = node.getData();
+	}
+	
+	/**
+	 * Retrieve Node Data
+	 * 
+	 * @return T data
+	 * @throws None
+	 */
+	public T getData() {
+		return this.data;
+	}
 }
