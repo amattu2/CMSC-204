39a5250e3edcac985457da19ed20182be272e95c
Alec M <alecmattu1234@gmail.com>
2020-04-08 08:29:31 -0400

    Renamed MorseCodeTree to specifications

---
+++ Project5/MorseCodeTree.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+/**
+ * A morse code tree class
+ * 
+ * @author Alec M.
+ * @date 04/08/2020
+ * @version 0.01
+ * @see MorseCodeTree(2).html
+ */
+public class MorseCodeTree implements LinkedConverterTreeInterface<String> {
+
+	@Override
+	public TreeNode<String> getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRoot(TreeNode<String> newNode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public LinkedConverterTreeInterface<String> insert(String code, String result) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNode(TreeNode<String> root, String code, String letter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String fetch(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String fetchNode(TreeNode<String> root, String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedConverterTreeInterface<String> delete(String data) throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedConverterTreeInterface<String> update() throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void buildTree() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<String> toArrayList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void LNRoutputTraversal(TreeNode<String> root, ArrayList<String> list) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
