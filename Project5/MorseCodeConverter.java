d381d719f9a3145ceeb18439f2022d5835b4020f
Alec M <alecmattu1234@gmail.com>
2020-04-13 18:13:22 -0400

    Implement printTree method

--- Project5/MorseCodeConverter.java
+++ Project5/MorseCodeConverter.java
@@ -2,21 +2,44 @@
  * A morse code converter utility
  * 
  * @author Alec M.
- * @date 04/07/2020
- * @version 0.01
+ * @date 04/13/2020
+ * @version 0.02
  * @see MorseCodeConverter(2).html
  */
 public class MorseCodeConverter {
-	public static String convertToEnglish(String text) {
+	// Class Variables
+	protected static MorseCodeTree tree = null;
+	
+	public static String convertToEnglish(String code) {
+		// Todo... parse code... etc. assign tree to the parsed tree
 		return null;
 	}
 
-	public static String convertToEnglish(java.io.File selectedFile) throws java.io.FileNotFoundException {
+	public static String convertToEnglish(java.io.File file) throws java.io.FileNotFoundException {
+		// Todo... parse code... etc. assign tree to the parsed tree
 		return null;
 	}
 
+	/**
+	 * Print A Previously Generated MorseCodeTree
+	 * 
+	 * @return String tree
+	 * @throws None
+	 */
 	public static String printTree() {
-		return null;
+		// Checks
+		if (tree == null) { return ""; }
+		
+		// Variables
+		java.util.ArrayList<String> list = tree.toArrayList();
+		String result = "";
+		
+		// Loop
+		for (String letter : list) {
+			result += letter + " ";
+		}
+		
+		// Return
+		return result.trim();
 	}
-
 }
