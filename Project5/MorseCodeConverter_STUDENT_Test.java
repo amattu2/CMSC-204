363e2c605e86bd48429fd17f19e8ef12b582ba8e
Alec M <alecmattu1234@gmail.com>
2020-04-13 17:59:43 -0400

    Added empty jUnit tests

---
+++ Project5/MorseCodeConverter_STUDENT_Test.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MorseCodeConverter_STUDENT_Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConvertToEnglishString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertToEnglishFile() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Project5/MorseCodeTree_STUDENT_Test.java
@@ -0,0 +1,72 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MorseCodeTree_STUDENT_Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetRoot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRoot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuildTree() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToArrayList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLNRoutputTraversal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+}
