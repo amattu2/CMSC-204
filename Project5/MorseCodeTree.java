5db3e49fcbdaf369e43be466a8e5d008c20e2463
Alec M <alecmattu1234@gmail.com>
2020-04-13 18:40:22 -0400

    Implement multiple methods

--- Project5/MorseCodeTree.java
+++ Project5/MorseCodeTree.java
@@ -1,21 +1,20 @@
-import java.util.ArrayList;
-
 /**
  * A morse code tree class
  * 
  * @author Alec M.
- * @date 04/08/2020
- * @version 0.01
+ * @date 04/13/2020
+ * @version 0.02a
  * @see MorseCodeTree(2).html
+ * @see LinkedConverterTreeInterface(2).html
  */
 public class MorseCodeTree implements LinkedConverterTreeInterface<String> {
-
-	@Override
-	public TreeNode<String> getRoot() {
-		// TODO Auto-generated method stub
-		return null;
+	// Class Variables
+	protected TreeNode<String> root = null;
+	
+	public MorseCodeTree() {
+		buildTree();
 	}
-
+	
 	@Override
 	public void setRoot(TreeNode<String> newNode) {
 		// TODO Auto-generated method stub
@@ -30,9 +29,21 @@ public class MorseCodeTree implements LinkedConverterTreeInterface<String> {
 
 	@Override
 	public void addNode(TreeNode<String> root, String code, String letter) {
-		// TODO Auto-generated method stub
+		this.addNode(root, code, letter);
 		
 	}
+	
+	/**
+	 * Get Root Node
+	 * 
+	 * @param None
+	 * @return TreeNode<String> root
+	 * @throws None
+	 */
+	@Override
+	public TreeNode<String> getRoot() {
+		return this.root;
+	}	
 
 	@Override
 	public String fetch(String code) {
@@ -46,34 +57,55 @@ public class MorseCodeTree implements LinkedConverterTreeInterface<String> {
 		return null;
 	}
 
+	/**
+	 * Convert Tree To ArrayList
+	 * 
+	 * @param None
+	 * @return ArrayList<String> sorted list
+	 * @throws None
+	 */
 	@Override
-	public LinkedConverterTreeInterface<String> delete(String data) throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public LinkedConverterTreeInterface<String> update() throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+	public java.util.ArrayList<String> toArrayList() {
+		// Variables
+		java.util.ArrayList<String> list = new java.util.ArrayList<String>();
+		
+		// Sort
+		this.LNRoutputTraversal(this.root, list);
+		
+		// Return
+		return list;
 	}
 
 	@Override
 	public void buildTree() {
 		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public ArrayList<String> toArrayList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void LNRoutputTraversal(TreeNode<String> root, ArrayList<String> list) {
-		// TODO Auto-generated method stub
-		
 	}
 	
+	/**
+	 * Recursively Sort Tree (LNR, In order)
+	 * 
+	 * @param TreeNode<String> node
+	 * @param ArrayList<String> list
+	 * @throws None
+	 */
+	@Override
+	public void LNRoutputTraversal(TreeNode<String> node, java.util.ArrayList<String> list) {
+		// Checks
+		if (node == null) { return; }
+		
+		// Sort
+		this.LNRoutputTraversal(node.getLeft(), list);
+		list.add(node.getData());
+		this.LNRoutputTraversal(node.getRight(), list);
+	}
+	
+	@Override
+	public LinkedConverterTreeInterface<String> delete(String data) throws UnsupportedOperationException {
+		throw new UnsupportedOperationException("Unsupported method");
+	}
+
+	@Override
+	public LinkedConverterTreeInterface<String> update() throws UnsupportedOperationException {
+		throw new UnsupportedOperationException("Unsupported method");
+	}
 }
