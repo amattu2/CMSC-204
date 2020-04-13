20ae468d0ce764ed565595f6be6271131f62748b
Alec M <alecmattu1234@gmail.com>
2020-04-13 19:00:17 -0400

    Implemented class methods

--- Project5/MorseCodeConverter.java
+++ Project5/MorseCodeConverter.java
@@ -8,16 +8,54 @@
  */
 public class MorseCodeConverter {
 	// Class Variables
-	protected static MorseCodeTree tree = null;
+	protected static MorseCodeTree tree = new MorseCodeTree();
 	
-	public static String convertToEnglish(String code) {
-		// Todo... parse code... etc. assign tree to the parsed tree
-		return null;
+	/**
+	 * Convert String Morse Code Into English
+	 * 
+	 * @param String encoded input
+	 * @return String decoded output
+	 * @throws None
+	 */
+	public static String convertToEnglish(String input) {
+		// Variables
+		String result = "";
+		String[] words = input.split(" / ");
+		
+		// Loops
+		for (String word : words) {
+			// Variables
+			String[] letters = word.split(" ");
+			
+			// Loops
+			for (String letter : letters) {
+				result += MorseCodeConverter.tree.fetch(letter);
+			}
+			
+			result += " ";
+		}
+		
+		// Return
+		return result.trim();
 	}
 
 	public static String convertToEnglish(java.io.File file) throws java.io.FileNotFoundException {
-		// Todo... parse code... etc. assign tree to the parsed tree
-		return null;
+		// Variables
+		java.util.ArrayList<String> lines = new java.util.ArrayList<String>();
+		java.util.Scanner scanner = new java.util.Scanner(file);
+		String result = "";
+		
+		// Loops
+		while (scanner.hasNext()) {
+			lines.add(scanner.nextLine());
+		}
+		for (int i = 0; i < lines.size(); i++) {
+			result += MorseCodeConverter.convertToEnglish(lines.get(i));
+		}
+		
+		// Return
+		scanner.close();
+		return result.trim();
 	}
 
 	/**
