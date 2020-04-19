4f77c8ef76341423c5470f584d7c2ee4cb4d3811
Alec M <38357871+amattu2@users.noreply.github.com>
2020-04-19 14:28:50 -0400

    Added Lab11

---
+++ Lab11/CarPanel.java
@@ -0,0 +1,62 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+/**
+   This component draws two car shapes.
+*/
+public class CarPanel extends JComponent
+{  
+	private Car car1;
+	private int x,y, delay;
+	private CarQueue carQueue;
+	private int direction;
+	
+	CarPanel(int x1, int y1, int d, CarQueue queue)
+	{
+		delay = d;
+        x=x1;
+        y=y1;
+        car1 = new Car(x, y, this);
+        carQueue = queue;
+	}
+	public void startAnimation()
+	   {
+	      class AnimationRunnable implements Runnable
+	      {
+	         public void run()
+	         {
+	            try
+	            {
+	               for(int i=0;i<10;i++)
+	               {
+	            	   direction = carQueue.deleteQueue();
+	            	   x = x+10;
+	            	   repaint();
+	            	   Thread.sleep(delay*1000);
+	            	   
+	               }
+	            }
+	            catch (InterruptedException exception)
+	            {
+	            	
+	            }
+	            finally
+	            {
+	            	
+	            }
+	         }
+	      }
+	      
+	      Runnable r = new AnimationRunnable();
+	      Thread t = new Thread(r);
+	      t.start();
+	   }
+	
+   public void paintComponent(Graphics g)
+   {  
+      Graphics2D g2 = (Graphics2D) g;
+
+      car1.draw(g2,x,y);    
+   }
+}
