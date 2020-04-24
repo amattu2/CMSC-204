79863e4832d357d52fed3aa76c3ee0fc0b62c649
= <alecmattu1234@gmail.com>
2020-04-24 18:40:06 -0400

    Implemented function bodies

--- Project6/Town.java
+++ Project6/Town.java
@@ -2,39 +2,94 @@
  * A town representation data class
  * 
  * @author Alec M.
- * @date 04/23/2020
- * @version 0.01a
+ * @date 04/24/2020
+ * @version 0.01b
  * @see Town.html
  */
 public class Town implements java.lang.Comparable<Town> {
-
+	// Class Variables
+	private String name = "";
+	
+	/**
+	 * Class Constructor
+	 * 
+	 * @param String name
+	 * @return this
+	 * @throws None
+	 */
 	public Town(String n) {
-
+		// Variables
+		this.name = n;
 	}
 
+	/**
+	 * Class Constructor
+	 * 
+	 * @param Town town
+	 * @return this
+	 * @throws None
+	 */
 	public Town(Town t) {
-		
+		// Variables
+		this.name = t.name;
 	}
 	
+	/**
+	 * Get Town Name
+	 * 
+	 * @param None
+	 * @return String name
+	 * @throws None
+	 */
 	public String getName() {
-		return null;
+		return this.name;
 	}
 	
+	/**
+	 * Get Town Hashcode
+	 * Generated from town name
+	 * 
+	 * @param None
+	 * @return Integer hashcode
+	 * @throws None
+	 */
 	public int hashcode() {
-		return 0;
+		return this.name.hashCode();
 	}
 	
+	/**
+	 * Stringify Town
+	 * 
+	 * @param None
+	 * @return None
+	 * @throws None
+	 */
 	public String toString() {
-		return null;
+		return this.getName();
 	}
 
+	/**
+	 * Compare Two Towns
+	 * 
+	 * @param Town town
+	 * @return Boolean equal
+	 * @throws None
+	 */
 	public boolean equals(Town t) {
 		return false;
 	}
 	
+	/**
+	 * Compare Two Towns
+	 * 0 = Equal
+	 * 1 = Not Equal (No way to determine -1/1)
+	 * 
+	 * @param Town town
+	 * @return Integer comparison
+	 * @throws None
+	 */
 	@Override
 	public int compareTo(Town t) {
-		return 0;
+		return this.name.equals(t.name) ? 0 : 1;
 	}
-
 }
